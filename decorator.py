ce7769fdde0a3d8e937bc02b17d82f94ef45f75e
Aripilli-Bhavana <aripilli.bhavana8@gmail.com>
2025-07-30 15:40:07 +0530

    Added generator

--- decorator.py
+++ decorator.py
@@ -2,9 +2,30 @@ def dist(d):
     def new_display():
         print("************")
         d()
+        print("************")
     return new_display
 
 @dis
 def display():
     print("Python Programming")
 display()
+
+
+
+def division(d):
+    def new_division(a,b):
+        if b==0:
+            return 0
+        else:
+            return d(a,b)
+    return new_division
+
+@division
+def div(a,b):
+    return a/b
+d=div(10,2)
+print(d)
+d=div(10,0)
+print(d)
+
+        
